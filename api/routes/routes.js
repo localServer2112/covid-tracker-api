92243debd76ef3078eb00eb8c51bfa1ab95c5dac
Fawale Timmy <fawaletimmy1@gmail..com>
2020-05-05 13:46:59 +0100

    few changes

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const router = express.Router({ automatic405: true });
-const DATA = require('../models/model');
+const dataModel = require('../models/model');
+const testModel = require('../models/tests');
 const tags = require('../scripts/script');
 
 // post data to the covid test server
-router.post('/cvd', (req, res) => {  
+router.post('/cvd:uid', (req, res) => {  
       res.status(200).json(tags.getCVDResult(req.body));
     });
 // post data to the rsl test server
-router.post('/rsl', (req, res) => {  
+router.post('/rsl:uid', async (req, res) => {  
+  const user = await dataModel.findOne(req.params);
+  const _rsldata =  await tags.getRSLResult(req.body);
+  
   res.status(200).json(tags.getRSLResult(req.body));
 });
 
 router.get('/id', (req,res) => {
   res.status(200).json(tags.getUID());
 });
+
+
+
 // get users by registration date
 router.get('/user',async (req, res)=>{
   try {
-      const users = await DATA.find().
+      const users = await dataModel.find().
       sort('-reg_date');
       res.json(users);
   } catch (error) {
@@ -32,9 +39,17 @@ router.get('/user',async (req, res)=>{
 // create new user
 router.post('/user', async (req,res) => {
   // using the model, we create a new user
-  const new_user = new DATA({
+  const new_user = new dataModel({
       userID : tags.getUID,
-      details : req.body
+      name: req.body.name,
+      phone: req.body.phone,
+      year: req.body.year,
+      sex: req.body.sex,
+      state: req.body.state,
+      city: req.body.city,
+      streetname: req.body.streetname,
+      occupation: req.body.occupation,
+      question: req.body.question,      
   });
   try{
   const savedUser = await new_user.save();
@@ -52,7 +67,7 @@ router.post('/user', async (req,res) => {
 
 // update the user details
 router.put('/user', async (req,res) => {
-  const updateUser = DATA.findOneAndUpdate(
+  const updateUser = dataModel.findOneAndUpdate(
       // the title of the item to find
       {userID : req.body.userID},
       {details : req.body},
